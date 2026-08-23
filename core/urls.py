a5410d640cf21773ea07a76ed3c9ff30135b7317
Rahuldev007-ai <rahulchanpura007@gmail.com>
2026-08-23 12:37:39 +0530

    Task 18 : admin can view ,edit and delete user details

--- core/urls.py
+++ core/urls.py
@@ -10,6 +10,10 @@ from .views import (
     admin_drivers,
     admin_users,
     admin_bookings,
+    admin_create_user,
+    admin_edit_user,
+    admin_delete_user,
+    admin_user_detail,
     )
 
 urlpatterns = [
@@ -27,6 +31,11 @@ urlpatterns = [
     path('drivers/', admin_drivers, name='admin_drivers'),
     path('users/', admin_users, name='admin_users'),
     path('bookings/', admin_bookings, name='admin_bookings'),
+    
+    path('users/create/', admin_create_user, name='admin_create_user'),
+    path('users/<int:user_id>/', admin_user_detail, name='admin_user_detail'),
+    path('users/<int:user_id>/edit/', admin_edit_user, name='admin_edit_user'),
+    path('users/<int:user_id>/delete/', admin_delete_user, name='admin_delete_user')    
 ]
 
 
--- core/views.py
+++ core/views.py
@@ -230,3 +230,15 @@ def admin_users(request):
 
 def admin_bookings(request):
     return render(request, 'admin_pages/bookings.html')
+
+def admin_create_user(request):
+    return render(request,"admin_pages/user_forms.html")
+
+def admin_edit_user(reqeust,user_id):
+    return render(reqeust,"admin_pages/user_forms.html")
+
+def admin_delete_user(request,user_id):
+    return render(request, 'admin_pages/user_confirm_delete.html')
+
+def admin_user_detail(request,user_id):
+    return render(request, 'admin_pages/user_detail.html')
