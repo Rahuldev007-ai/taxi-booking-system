d0e0aa3ebb3dd39f0e78823421b63f7f19191264
Rahuldev007-ai <rahulchanpura007@gmail.com>
2026-08-23 11:43:08 +0530

    Task 16 : Create a User model with fields like name, email, mobile, password.

--- core/admin.py
+++ core/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import AdminUser
+from .models import AdminUser,User
 # Register your models here.
 
 admin.site.register(AdminUser)
+admin.site.register(User)
